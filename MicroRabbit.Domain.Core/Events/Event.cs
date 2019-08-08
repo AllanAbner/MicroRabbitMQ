dc7848b0e504f838909745a7708395d99922dbea
allanabner <allanabner_ti@hotmail.com>
2019-08-07 22:15:54 -0300

    Implemented interfaces and classes for Domain.Core (CQRS)

---
+++ MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime TimeStamp { get; private set; }
+
+        protected Event()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
