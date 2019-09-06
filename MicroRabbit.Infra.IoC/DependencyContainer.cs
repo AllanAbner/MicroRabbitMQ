efde9607a61a33713709fe21cde41a824938454b
allanabner <allanabner_ti@hotmail.com>
2019-09-05 22:39:46 -0300

    banking microservices layer

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void Register(IServiceCollection services)
+        {
+            //Domain bus
+
+            services.AddTransient<IEventBus,RabbitMQBus>
+        }
+    }
+}
